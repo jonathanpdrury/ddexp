8a701231f8b7e76c19192b3bb19a1462bcc94a68
Jonathan Drury <jonathan.p.drury@gmail.com>
2018-08-01 12:15:55 +0100

    various testing scripts

---
+++ tests/20180801/output_processing.r
@@ -0,0 +1,11 @@
+tf<-read.csv("~/ddexp/tests/20180801/tanager_fits_fruit_TESTS.csv")
+
+par(mfrow=c(2,2))
+plot(tf$DDexpgeo_RPANDA.lnL,tf$DDexpgeo_mvMORPH.lnL)
+abline(a=0,b=1,col="red")
+plot(tf$DDexpgeo_RPANDA.r,tf$DDexpgeo_mvMORPH.r)
+abline(a=0,b=1,col="red")
+plot(sqrt(tf$DDexpgeo_RPANDA.sig2),sqrt(tf$DDexpgeo_mvMORPH.sig2))
+abline(a=0,b=1,col="red")
+plot(tf$DDexpgeo_RPANDA.z0,tf$DDexpgeo_mvMORPH.z0)
+abline(a=0,b=1,col="red")
